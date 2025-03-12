7cf685eab4bd31af2ed778bf7c4de96af9450cf5
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2025-03-12 16:55:21 +0200

    Ex 8.2

--- index.js
+++ index.js
@@ -94,9 +94,19 @@ let books = [
 ];
 
 const typeDefs = `
+	type Book {
+		title: String!
+		published: Int!
+		author: String!
+		id: ID!
+		genres: [String!]!
+	}
+
+
   type Query {
     bookCount: Int
     authorCount: Int
+    allBooks: [Book!]!
   }
 `;
 
@@ -104,6 +114,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 };
 
